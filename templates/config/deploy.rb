1c7abfde5a01ff07412c096bf719d1a14dd3a12a
Denis Barushev <barushev@gmail.com>
2008-08-06 08:07:01 +0300

    Modified default deploy.rb.

--- templates/config/deploy.rb
+++ templates/config/deploy.rb
@@ -1,14 +1,15 @@
+require "mongrel_cluster/recipes"
+
 set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :repository,  "someuser@somehost:git/#{application}.git"
+set :scm, :git
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :user, "deploy"
+set :use_sudo, false
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :deploy_to, "/var/www/apps/#{application}"
+
+set :mongrel_conf, "#{deploy_to}/current/config/mongrel_cluster.yml"
 
 role :app, "your app-server here"
 role :web, "your web-server here"
