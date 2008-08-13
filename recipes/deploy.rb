fd6f9b839b98c2ff806119f92e4616a74b1d92dc
Denis Barushev <barushev@gmail.com>
2008-08-13 18:18:42 +0300

    Added some deploy recipes.

---
+++ recipes/deploy.rb
@@ -0,0 +1,20 @@
+namespace :capone do
+  namespace :deploy do
+    desc <<-DESC
+      Create MySQL user and database.
+    DESC
+    task :setup_db, :roles => :db, :only => { :primary => true } do
+      run "echo 'Not implemented yet.'"
+    end
+
+    desc <<-DESC
+      Install gems needed by application.
+    DESC
+    task :install_gems, :roles => :app do
+      sudo "rake gems:install -f #{current_path}/Rakefile"
+    end
+  end
+end
+
+after "deploy:setup", "capone:deploy:setup_db"
+after "deploy:update_code", "capone:deploy:install_gems"
